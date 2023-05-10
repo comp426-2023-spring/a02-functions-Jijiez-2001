a7d11b6bf9c6d6553e52b6cd3233eb7477198dc6
Jijie Zhou <jijiez@email.unc.edu>
2023-05-10 18:34:09 -0400

    rewrite cli.js

--- cli.js
+++ cli.js
@@ -1,73 +1,101 @@
-#!/usr/bin/env node
-import moment from "moment-timezone"
-import minimist from "minimist"
-import fetch from "node-fetch"
+#!/usr/bin / env node
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-05-10 14:29:24
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-05-10 18:32:59
+ */
 
-
-const help_message = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE" + "\n" +
-"-h            Show this help message and exit." + "\n" + 
-"-n, -s        Latitude: N positive; S negative." + "\n" +
-"-e, -w        Longitude: E positive; W negative." + "\n" +
-"-z            Time zone: uses tz.guess() from moment-timezone by default." + "\n" +
-"-d 0-6        Day to retrieve weather: 0 is today; defaults to 1." + "\n" +
-"-j            Echo pretty JSON from open-meteo API and exit." 
+const fetch = require('node-fetch');
+const moment = require('moment-timezone');
+const minimist = require('minimist');
 
 const args = minimist(process.argv.slice(2));
-var latitude = 99999;
-var longitude = 99999;
-
-if (args.n > 0) {
-  latitude= args.n;
-}
-if (args.s) {
-  latitude = (-1) * args.s;
-}
-if (args.e > 0) {
-  longitude = args.e;
-}
-if (args.w) {
-  longitude = (-1) * args.w;
-}
-
-
-const timezone = args.z || moment.tz.guess();
-const days = args.d
 
 if (args.h) {
-  console.log(help_message);
+  console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.`);
   process.exit(0);
 }
 
+let timezone = moment.tz.guess();
+if (args.z) {
+  timezone = args.z;
+}
 
-// Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude+ '&daily=precipitation_hours&timezone=' + timezone);
-// Get the data from the request
-const data = await response.json();
+let latitude, longitude;
 
-
-if (args.j) {
-  console.log(data);
+if (args.n && args.s) {
+  console.log("Only one latitude");
+  process.exit(0);
+}
+else if (args.n) {
+  latitude = args.n;
+}
+else if (args.s) {
+  latitude = -args.s;
+}
+else {
+  console.log("Latitude must be in range");
   process.exit(0);
 }
 
-if (longitude > 90 || latitude > 90) {
-  process.exit(1)
+if (args.e && args.w) {
+  console.log("Only one longitude value");
+  process.exit(0);
+}
+else if (args.e) {
+  longitude = args.e;
+}
+else if (args.w) {
+  longitude = -args.w;
+}
+else {
+  console.log("Longitude must be in range");
+  process.exit(0);
 }
 
-let galosh = ""
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=precipitation_hours`;
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const days = args.d || 1;
+    let string;
 
-if (data.daily.precipitation_hours != null) {
-  if (data.daily.precipitation_hours[days] > 0) {
-    galosh = "You might need your galoshes "
-  } else {
-    galosh = "You will not need your galoshes "
-  }
-}
+    if (data.daily.precipitation_hours[days] == 0) {
+      string = "You will not need your galoshes ";
+    } else {
+      string = "You might need your galoshes ";
+    }
 
-if (days == 0) {
-    console.log(galosh + "today.")
-  } else if (days > 1) {
-    console.log(galosh + "in " + days + " days.")
-  } else {
-    console.log(galosh + "tomorrow.")
-  } 
+    if (days == 0) {
+      string += "today.";
+    } else if (days > 1) {
+      string += `in ${days} days.`;
+    } else {
+      string += "tomorrow.";
+    }
+
+    if (args.j) {
+      console.log(JSON.stringify(data, null, 2));
+    } else {
+      console.log(string);
+    }
+  })
+  .catch(error => console.error(error));
+
+
+
+
+
+
+
+
+
