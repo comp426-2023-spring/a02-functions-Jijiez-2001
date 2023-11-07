33f5c79ec0ef57cf2083672c156aa62986c36d4e
jijiezhou <jijiez@email.unc.edu>
2023-11-07 01:02:11 -0500

    keep editing

--- cli.js
+++ cli.js
@@ -4,62 +4,28 @@ import minimist from "minimist";
 import moment from "moment-timezone";
 import fetch from "node-fetch";
 
-// Parse the command line arguments
 const args = minimist(process.argv.slice(2));
 
-// Help flag
 if (args.h) {
-  console.log(`Usage: galosh.js [options]
-Options:
-  -h            Show this help message and exit.
-  -n, -s        Latitude: N positive; S negative.
-  -e, -w        Longitude: E positive; W negative.
-  -z            Time zone: uses tz.guess() from moment-timezone by default.
-  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-  -j            Echo pretty JSON from open-meteo API and exit.`);
+  console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+        -h            Show this help message and exit.
+        -n, -s        Latitude: N positive; S negative.
+        -e, -w        Longitude: E positive; W negative.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
+        -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+        -j            Echo pretty JSON from open-meteo API and exit.`);
   process.exit(0);
 }
 
-// If -j is present, we skip latitude and longitude checks
-if (args.j) {
-  const timezone = moment.tz.guess();
-  const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=0&longitude=0&timezone=" +
-    timezone;
-
-  // Fetch the weather data
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  process.exit(0);
+//timezone
+let timezone = moment.tz.guess();
+if (args.z) {
+  timezone = args.z;
 }
 
-// Time zone
-const timezone = args.z ? args.z : moment.tz.guess();
+let latitude = args.n || -args.s;
+let longitude = args.e || -args.w;
 
-// Latitude
-let latitude;
-if (args.n) {
-  latitude = args.n;
-} else if (args.s) {
-  latitude = -args.s;
-} else {
-  console.error("Error: Please enter a valid latitude using -n or -s.");
-  process.exit(1);
-}
-
-// Longitude
-let longitude;
-if (args.e) {
-  longitude = args.e;
-} else if (args.w) {
-  longitude = -args.w;
-} else {
-  console.error("Error: Please enter a valid longitude using -e or -w.");
-  process.exit(1);
-}
-
-// Build the API URL
 const url =
   "https://api.open-meteo.com/v1/forecast?latitude=" +
   latitude +
@@ -69,7 +35,20 @@ const url =
   timezone +
   "&daily=precipitation_hours&current_weather=true";
 
-// Fetch the weather data
 const response = await fetch(url);
 const data = await response.json();
-console.log(data);
+
+if (args.j) {
+  console.log(data);
+  process.exit(0);
+}
+
+//days
+const days = args.d;
+if (days == 0) {
+  console.log("today.");
+} else if (days > 1) {
+  console.log("in " + days + " days.");
+} else {
+  console.log("tomorrow.");
+}
